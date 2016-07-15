32eab4f5cdfddaf8c8d8697984416b359f85d92f
Jaime Gomes <jaimegomes.bjj@gmail.com>
2016-07-14 21:49:17 -0300

    Upload do arquivo

--- CreditMiner/src/main/java/br/com/bjjsolutions/mb/ConfiguracaoMB.java
+++ CreditMiner/src/main/java/br/com/bjjsolutions/mb/ConfiguracaoMB.java
@@ -1,5 +1,7 @@
 package br.com.bjjsolutions.mb;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -15,16 +17,31 @@ public class ConfiguracaoMB {
 	private static final String PATH_CONTENT_LOGIN = "pages/login_content.xhtml";
 	private static final String PATH_CONTENT_CONFIGURACAO = "pages/configuracao_content.xhtml";
 
+	@SuppressWarnings("resource")
 	public void upload() {
 		try {
 			if (file != null) {
-				new Scanner(file.getInputStream()).useDelimiter("\\A").next();
+				String conteudo = new Scanner(file.getInputStream()).useDelimiter("\\A").next();
+				salvarArquivo(conteudo, getFileNameFromPart(file));
+
 			}
 		} catch (IOException e) {
-
+			e.printStackTrace();
 		}
 	}
 
+	private void salvarArquivo(String conteudo, String nomeArquivo) {
+		FileWriter arquivo;
+		try {
+			arquivo = new FileWriter(new File("/home/CreditMiner/leitura/" + nomeArquivo));
+			arquivo.write(conteudo);
+			arquivo.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	public Part getFile() {
 		return file;
 	}
@@ -42,5 +59,16 @@ public class ConfiguracaoMB {
 
 	public static void setIsLogin(boolean isLoginP) {
 		isLogin = isLoginP;
+	}
+
+	private static String getFileNameFromPart(Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		for (String content : partHeader.split(";")) {
+			if (content.trim().startsWith("filename")) {
+				String fileName = content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+				return fileName;
+			}
+		}
+		return null;
 	}
 }
