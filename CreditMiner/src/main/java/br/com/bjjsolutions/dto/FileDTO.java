750a75f9858a03705c76949da969b9bdf4f2138d
Jaime Gomes <jaimegomes.bjj@gmail.com>
2016-07-16 17:29:03 -0300

    criação FileDTO e metodo excluir FileCOntroller

---
+++ CreditMiner/src/main/java/br/com/bjjsolutions/dto/FileDTO.java
@@ -0,0 +1,26 @@
+package br.com.bjjsolutions.dto;
+
+public class FileDTO {
+
+	private String fileName;
+
+	public FileDTO() {
+		super();
+	}
+
+	/**
+	 * @return the fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @param fileName
+	 *            the fileName to set
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+}
